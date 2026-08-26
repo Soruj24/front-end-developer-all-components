fca98ed318c1fc61cc5a1172c208f6f39525d9c0
Soruj Mahmud <villagecode868@gmai.com>
2026-08-26 23:07:16 +0600

    refactor: remove SidebarHeader component to simplify Sidebar structure

--- components/layout/sidebar/Sidebar.tsx
+++ components/layout/sidebar/Sidebar.tsx
@@ -15,7 +15,6 @@ import {
   TEXT,
   COLOR,
 } from "@/constants/tokens";
-import { SidebarHeader } from "./SidebarHeader";
 import { SidebarSearch } from "./SidebarSearch";
 import { SidebarNavigation } from "./SidebarNavigation";
 import { SidebarFooter } from "./SidebarFooter";
@@ -118,10 +117,7 @@ export function Sidebar({ sections, width = LAYOUT.sidebarWidth }: SidebarProps)
         style={{ width: collapsed ? LAYOUT.sidebarCollapsedWidth : width }}
         aria-label="Documentation"
       >
-        <SidebarHeader
-          collapsed={collapsed}
-          onToggleCollapse={() => setCollapsed((p) => !p)}
-        />
+         
 
         {!collapsed && (
           <SidebarSearch value={search} onChange={setSearch} />
